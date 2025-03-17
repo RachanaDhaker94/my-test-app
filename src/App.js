d4b55c3835276bb2ee49391493ff84fa5c4c8baa
Rachana Dhaker <rachana.dhaker@ibm.com>
2025-03-17 17:34:18 +0530

    remove style from li

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 function App() {
   const [data, setData] = React.useState([]);
+  const [currentPage, setCurrentPage] = React.useState(1);
 
   React.useEffect(() => {
     fetch("https://picsum.photos/v2/list?limit=10")
@@ -11,17 +12,52 @@ function App() {
       .then((res) => setData(res));
   }, []);
 
+  const itemsPerPage = 1;
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  
+  const currentItems = data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+ 
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+ 
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+
   return (
     <div className="App">
       <header className="">
         <h1>ALL Content</h1>
-        <ul>
-          {data.length > 0 &&
-            data.map((item, index) => {
-              return (<li key={item.id}><img src={item?.url} alt='sdsd' /></li>);
+        <ul style={{listStyle:'none'}}>
+          {currentItems.length > 0 &&
+            currentItems.map((item, index) => {
+              return (<li key={item.id} >
+                          <img src={item?.download_url}
+                           alt="image"  
+                           width={item?.width}
+                           height={item?.height}/>
+                            </li>);
             })}
         </ul>
-        <div><button>Next</button></div>
+        <div>
+        <button onClick={prevPage} disabled={currentPage === 1} style={{marginRight:10}}>
+          Previous
+        </button>
+
+        <button onClick={nextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+        </div>
       </header>
     </div>
   );
